414bb42de19f03a0b5b7731ad6cb77e5b6f506c6
jisungkim11 <31737506+jisungkim11@users.noreply.github.com>
2017-09-07 12:09:06 -0400

    Update HelloWorld.java

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main (String [] args){
 		System.out.println("Hello World");
 		System.out.println("making adjustments for github");
+		System.out.println("adding code from git hub!");
 	}
 }
